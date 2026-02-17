4705559bab1c68e42de2702e32e6bf9e9d91c801
Michele Facco <faccomichele@gmail.com>
2026-02-17 18:34:22 +0700

    test fix p2

--- data.tf
+++ data.tf
@@ -3,6 +3,11 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-# data "http" "metadata" {
-#   url = "${aws_apigatewayv2_stage.saml.invoke_url}/metadata"
-# }
+data "http" "metadata" {
+  url = "${aws_apigatewayv2_stage.saml.invoke_url}/metadata"
+  depends_on = [
+    aws_apigatewayv2_api.saml,
+    aws_lambda_function.saml_processor,
+    aws_lambda_permission.api_gateway
+  ]
+}
