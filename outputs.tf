6f921d953cb491b5032f26c46b370812177d4a65
Michele Facco <faccomichele@gmail.com>
2026-02-18 11:01:44 +0700

    restoring required outputs

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,15 @@ output "api_gateway_url" {
 
 output "login_page_url" {
   description = "URL to access the login page (CloudFront recommended for HTTPS access)"
-  value       = "https://${aws_cloudfront_distribution.login_page.domain_name}/index.html"
+  value       = "https://${aws_cloudfront_distribution.login_page.domain_name}"
+}
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront distribution ID"
+  value       = aws_cloudfront_distribution.login_page.id
+}
+
+output "website_bucket_name" {
+  description = "S3 bucket name for login page"
+  value       = aws_s3_bucket.login_page.id
 }
