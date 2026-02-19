6132e1cbf14cba8500fcd35e4095f1fc6c6be023
Michele Facco <faccomichele@gmail.com>
2026-02-19 12:52:26 +0700

    Update data.tf

--- data.tf
+++ data.tf
@@ -5,6 +5,13 @@ data "aws_region" "current" {}
 
 data "http" "metadata" {
   url = "${aws_apigatewayv2_stage.saml.invoke_url}/metadata"
+  
+  retry {
+    attempts = 2
+    min_delay_ms = 60000 # 1 min
+    max_delay_ms = 60000 # 1 min
+  }
+
   depends_on = [
     aws_apigatewayv2_api.saml,
     aws_lambda_function.saml_processor,
