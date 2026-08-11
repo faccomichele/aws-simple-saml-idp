2523882d3bb6a55e4cb73b0f4675d0922ef95138
Michele Facco <faccomichele@gmail.com>
2026-08-11 12:22:47 +0700

    fix

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -3,12 +3,10 @@ resource "aws_cloudwatch_log_group" "lambda_logs" {
   name              = "/aws/lambda/${aws_lambda_function.saml_processor.function_name}"
   retention_in_days = local.log_retention_days
 
-  tags = merge(local.tags,
-    {
-      Name = "${local.project_alias}-lambda-logs-${local.environment}"
-      RepositoryFile = "cloudwatch.tf"
-    }
-  )
+  tags = {
+    Name = "${local.project_alias}-lambda-logs-${local.environment}"
+    RepositoryFile = "cloudwatch.tf"
+  }
 }
 
 # CloudWatch Log Group for API Gateway
@@ -16,10 +14,8 @@ resource "aws_cloudwatch_log_group" "api_logs" {
   name              = "/aws/apigateway/${local.project_alias}-${local.environment}"
   retention_in_days = local.log_retention_days
 
-  tags = merge(local.tags,
-    {
-      Name = "${local.project_alias}-api-logs-${local.environment}"
-      RepositoryFile = "cloudwatch.tf"
-    }
-  )
+  tags ={
+    Name = "${local.project_alias}-api-logs-${local.environment}"
+    RepositoryFile = "cloudwatch.tf"
+  }
 }
